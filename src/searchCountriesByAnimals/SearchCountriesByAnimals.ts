bc5fbad29162d57552edc8994c325f972eb6d0c7
Guillaume Longuet <guillaume.longuet@labanqui.se>
2023-04-17 21:50:31 +0200

    feat(search-countries-by-animals): :sparkles: add search usecase

---
+++ src/searchCountriesByAnimals/SearchCountriesByAnimals.ts
@@ -0,0 +1,57 @@
+import { Animal } from '@entities/Animal'
+import { Country } from '@entities/Country'
+import { GetCountriesPort } from './GetCountriesPort'
+import { Person } from '@entities/Person'
+import { SearchCountriesByAnimalsRequest } from './SearchCountriesByAnimalsRequest'
+import { Usecase } from 'src/usecase/Usecase'
+
+export class SearchCountriesByAnimals
+  implements Usecase<SearchCountriesByAnimalsRequest, Country[]>
+{
+  constructor(private readonly getCountriesPort: GetCountriesPort) {}
+
+  execute = ({ animalName }: SearchCountriesByAnimalsRequest): Country[] => {
+    const countries = this.getCountriesPort.getCountries()
+
+    // return countries.filter(country => country.people.some(people => people.animals.     ))
+
+    return countries.reduce((filteredCountries, currentCountry) => {
+      const matchingAnimalPeople = this.getPeopleWithMatchingAnimal(
+        currentCountry.people,
+        animalName,
+      )
+
+      matchingAnimalPeople.length &&
+        filteredCountries.push({
+          ...currentCountry,
+          people: matchingAnimalPeople,
+        })
+
+      return filteredCountries
+    }, [] as Country[])
+  }
+
+  private getPeopleWithMatchingAnimal = (
+    people: Person[],
+    animalName: string,
+  ): Person[] => {
+    return people.reduce((matchingPeople, currentPerson) => {
+      const animals = this.getMatchingAnimals(currentPerson, animalName)
+
+      if (animals.length) {
+        matchingPeople.push({
+          ...currentPerson,
+          animals,
+        })
+      }
+      return matchingPeople
+    }, [] as Person[])
+  }
+
+  private getMatchingAnimals(
+    currentPerson: Person,
+    animalName: string,
+  ): Animal[] {
+    return currentPerson.animals.filter(({ name }) => name.includes(animalName))
+  }
+}
---
+++ src/searchCountriesByAnimals/SearchCountriesByAnimalsRequest.ts
@@ -0,0 +1,3 @@
+export type SearchCountriesByAnimalsRequest = {
+  animalName: string
+}
